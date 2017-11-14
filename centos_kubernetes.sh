9536909f91ccf3ab38d60499ed7e7c2e8283e84b
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2017-11-14 12:20:19 +0800

    Update centos_kubernetes.sh

--- centos_kubernetes.sh
+++ centos_kubernetes.sh
@@ -10,9 +10,13 @@
 
 yum install -y epel-release ;
 sed -e 's!^mirrorlist=!#mirrorlist=!g' -e 's!^#baseurl=!baseurl=!g'   -e 's!//download\.fedoraproject\.org/pub!//mirrors.ustc.edu.cn!g'  -e 's!http://mirrors\.ustc!https://mirrors.ustc!g'  -i /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo ;
+
+curl https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/kubernetes_repo > /etc/yum.repos.d/virt7-docker-common-release.repo ;
+
 yum makecache ;
 yum update -y;
-yum install htop net-tools etcd htop vim e4fsprogs iptables kubernetes openssh-server   byobu git zsh -y ;
+
+yum install --enablerepo=virt7-docker-common-release htop net-tools etcd htop vim e4fsprogs iptables kubernetes openssh-server   byobu git zsh flannel -y ;
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" ;
 
@@ -25,9 +29,8 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 for SERVICES in ` ls /usr/lib/systemd/system/kub* | awk -F '/' '{print $6}' ` docker etcd ; do systemctl restart $SERVICES ; systemctl enable  $SERVICES ; systemctl status $SERVICES ; systemctl start $SERVICES ; done ;
 
+echo "sleep 2 s ,wait service loading!"
 sleep 2;
-
-for SERVICES in ` ls /usr/lib/systemd/system/kub* | awk -F '/' '{print $6}' ` docker etcd ; do systemctl restart $SERVICES ;  systemctl status $SERVICES ; done ;
 
 ## test kubectl
 kubectl get nodes ;
