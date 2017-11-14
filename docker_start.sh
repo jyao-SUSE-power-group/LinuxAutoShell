5590cb8f1827fe110b536308a30504823d88a079
jyao <351022095@qq.com>
2017-11-14 15:16:18 +0800

    some changes

--- docker_start.sh
+++ docker_start.sh
@@ -10,3 +10,8 @@ docker run -d --name=ctj2 -h=master --restart=always -e "container=docker" --pri
 
 
 docker run -d --name=ctj3 -h=slave --link=ctj2:kube-master --restart=always -e "container=docker" --privileged=true   -v /sys/fs/cgroup:/sys/fs/cgroup   centos /usr/sbin/init
+
+
+
+
+for SERVICES in ` ls /usr/lib/systemd/system/kub* | awk -F '/' '{print $6}' ` docker etcd ; do systemctl stop $SERVICES ; done ;
