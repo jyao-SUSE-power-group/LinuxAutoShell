f91229bbf54324e7f101171aa3c37f075f675f44
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2017-12-28 12:37:16 +0800

    Update sogou-input.sh

--- archlinux/sogou-input.sh
+++ archlinux/sogou-input.sh
@@ -8,11 +8,23 @@ cp ~/.xprofile ~/.xprofile.bak
 
 cat << EOF >>  ~/.xprofile
 
+export LC_ALL=zh_CN.UTF-8
+
+export XIM=fcitx  
+
+export XIM_PROGRAM=fcitx
+
 export GTK_IM_MODULE=fcitx
 
 export QT_IM_MODULE=fcitx
 
 export XMODIFIERS="@im=fcitx"
+
+eval `dbus-launch --sh-syntax --exit-with-session`
+
+exec fcitx &
+
+
 
 EOF
 
