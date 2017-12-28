627d6048805f01122e47548091f3e15e3882eaee
jyao <jackyao713911@gmail.com>
2017-12-28 11:02:39 +0800

    mysql docker

--- docker _mysql.sh
+++ docker _mysql.sh
@@ -3,15 +3,17 @@
 
 
 sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json << EOF
+sudo cat << EOF >> /etc/docker/daemon.json
 {
   "registry-mirrors": ["https://0e3kttqp.mirror.aliyuncs.com"]
 }
 EOF
+
+
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 sudo mkdir -p /etc/docker_mysql
-curl https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/my.cnf > /etc/docker_mysql/my.cnf
+sh -c ' curl https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/my.cnf > /etc/docker_mysql/my.cnf '
 
-docker run --name mymysql --restart=always -d -p 3306:3306 -v /etc/docker_mysql/my.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf -v /etc/docker_mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root  mysql
+sudo docker run --name mymysql --restart=always -d -p 3306:3306 -v /etc/docker_mysql/my.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf -v /etc/docker_mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root  mysql
