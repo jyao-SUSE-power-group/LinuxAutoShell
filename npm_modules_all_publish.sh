4b615f383be26c92328d63288282e27e9f419771
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2020-04-11 21:25:16 +0800

    Update npm_modules_all_publish.sh

--- npm_modules_all_publish.sh
+++ npm_modules_all_publish.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+## cd node_modules
+## curl -s https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/npm_modules_all_publish.sh | bash -s
 # before 
 npm config set ignore_prepublish true
 for json in `find -name package.json`
