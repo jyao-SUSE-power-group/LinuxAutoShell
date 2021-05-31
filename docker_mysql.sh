b8e69c6956cbecbfa749e9fe15815132308e7371
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2021-05-31 09:36:03 +0800

    Update docker_mysql.sh

--- docker_mysql.sh
+++ docker_mysql.sh
@@ -20,7 +20,8 @@ EOF'
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-sudo mkdir -p /etc/docker_mysql
-sudo sh -c ' curl https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/my.cnf > /etc/docker_mysql/my.cnf '
+sudo mkdir -p /etc/docker_mysql/conf
+sudo mkdir -p /etc/docker_mysql/data
+sudo sh -c ' curl https://raw.githubusercontent.com/jyao-SUSE-power-group/LinuxAutoShell/master/my.cnf > /etc/docker_mysql/conf/my.cnf '
 
-sudo docker run --name my2  -e TZ="Asia/Shanghai" -v /etc/localtime:/etc/localtime:ro --restart=always -d -p 3307:3306 -v /etc/docker_mysql/my.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf  -e MYSQL_ROOT_PASSWORD=root  mysql
+sudo docker run --name my2  -e TZ="Asia/Shanghai" -v /etc/localtime:/etc/localtime:ro --restart=always -d -p 43307:3306 -v /etc/docker_mysql/data:/var/lib/mysql -v /etc/docker_mysql/conf/my.cnf:/etc/mysql/mysql.conf.d/my_custom.cnf  -e MYSQL_ROOT_PASSWORD=root@f/a-18  mysql
